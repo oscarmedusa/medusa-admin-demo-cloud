3d111b425cf8d6d20fd7002b177705b4b922acee
olivermrbl <oliver@mrbltech.com>
2025-01-21 13:29:59 -0500

    chore: Remove file plugin temporarily

--- medusa-config.ts
+++ medusa-config.ts
@@ -1,4 +1,4 @@
-import { loadEnv, defineConfig, Modules } from "@medusajs/framework/utils";
+import { defineConfig, loadEnv } from "@medusajs/framework/utils";
 
 loadEnv(process.env.NODE_ENV!, process.cwd());
 
@@ -16,26 +16,26 @@ export default defineConfig({
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     },
   },
-  modules: {
-    [Modules.FILE]: {
-      resolve: "@medusajs/medusa/file",
-      key: Modules.FILE,
-      options: {
-        providers: [
-          {
-            resolve: "@medusajs/medusa/file-s3",
-            id: "s3",
-            options: {
-              file_url: process.env.S3_FILE_URL,
-              access_key_id: process.env.S3_ACCESS_KEY_ID,
-              secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
-              region: process.env.S3_REGION,
-              bucket: process.env.S3_BUCKET,
-              endpoint: process.env.S3_ENDPOINT,
-            },
-          },
-        ],
-      },
-    },
-  },
+  // modules: {
+  //   [Modules.FILE]: {
+  //     resolve: "@medusajs/medusa/file",
+  //     key: Modules.FILE,
+  //     options: {
+  //       providers: [
+  //         {
+  //           resolve: "@medusajs/medusa/file-s3",
+  //           id: "s3",
+  //           options: {
+  //             file_url: process.env.S3_FILE_URL,
+  //             access_key_id: process.env.S3_ACCESS_KEY_ID,
+  //             secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
+  //             region: process.env.S3_REGION,
+  //             bucket: process.env.S3_BUCKET,
+  //             endpoint: process.env.S3_ENDPOINT,
+  //           },
+  //         },
+  //       ],
+  //     },
+  //   },
+  // },
 });
